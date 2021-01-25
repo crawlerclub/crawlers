c5a6feb467787f984b93f5dd9048a64d91004a2d
liuzl <liang@zliu.org>
2021-01-25 21:17:52 +0800

    ydl.com

---
+++ ydl.com/questions.py
@@ -0,0 +1,13 @@
+import requests
+from lsm import LSM
+db = LSM("questions.ldb")
+for i in range(1, 100000):
+    if i not in db:
+        url = "https://www.ydl.com/ask/%d" % i
+        print(url)
+        ret = requests.get(url)
+        if ret.status_code == 200:
+            db[i] = ret.text
+        else:
+            print(ret.status_code)
+            db[i] = "404"
