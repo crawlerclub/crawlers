3e8e37fae613c1693561f26e5d724a200dbd41bb
liuzl <liang@zliu.org>
2021-01-26 09:35:30 +0800

    update range

--- ydl.com/questions.py
+++ ydl.com/questions.py
@@ -1,7 +1,8 @@
 import requests
 from lsm import LSM
 db = LSM("questions.ldb")
-for i in range(1, 100000):
+#for i in range(1, 100000):
+for i in range(100000, 200000):
     if i not in db:
         url = "https://www.ydl.com/ask/%d" % i
         print(url)
