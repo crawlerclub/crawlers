84784f876773799b47b32e1ee0ff7230a8ab8cc0
liuzl <liang@zliu.org>
2021-01-26 12:19:46 +0800

    update ydl.com/questions.py

--- ydl.com/questions.py
+++ ydl.com/questions.py
@@ -1,14 +1,20 @@
 import requests
 from lsm import LSM
 db = LSM("questions.ldb")
+fail = 0
 #for i in range(1, 100000):
-for i in range(100000, 200000):
+#for i in range(100000, 200000):
+for i in range(302000, 402000):
     if i not in db:
         url = "https://www.ydl.com/ask/%d" % i
         print(url)
         ret = requests.get(url)
         if ret.status_code == 200:
             db[i] = ret.text
+            fail = 0
         else:
             print(ret.status_code)
+            fail += 1
+            if fail > 100 and fail % 10 == 0:
+                print("fail=%d" % fail)
             db[i] = "404"
