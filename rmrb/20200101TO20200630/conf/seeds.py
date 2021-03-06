d936e5fda48173ac7d04c502a39ee77fbb21a946
liuzl <liang@zliu.org>
2021-03-06 21:31:17 +0800

    update

---
+++ rmrb/20200101TO20200630/conf/seeds.py
@@ -0,0 +1,16 @@
+import datetime
+url_ = "http://paper.people.com.cn/rmrb/html/%s/nbs.D110000renmrb_01.htm"
+begin = datetime.date(2020, 1, 1)
+end = datetime.date(2020, 6, 30)
+d = begin
+delta = datetime.timedelta(days=1)
+urls = []
+while d <= end:
+    day = d.strftime("%Y-%m/%d")
+    url = url_ % day
+    d += delta
+    urls.append(url)
+seeds = [{"parser_name": "day", "url": x} for x in urls]
+import json
+ret = json.dumps(seeds, indent=2)
+print(ret)
--- rmrb/run.sh
+++ rmrb/run.sh
@@ -1,2 +1,8 @@
 #go get crawler.club/bcrawler
-bcrawler -log_dir ./log -alsologtostderr
+CURR_PATH=`cd $(dirname $0);pwd;`
+cd $CURR_PATH
+ts=`date +%Y-%m/%d`
+url="http://paper.people.com.cn/rmrb/html/$ts/nbs.D110000renmrb_01.htm"
+echo $url
+rm first.lock
+bcrawler -start day -start_url $url -log_dir ./log -alsologtostderr
